6a39bab38bd6094346189cf49bc08527bb96312d
charansaigompa <gompacharansai10@gmail.com>
2026-03-08 18:54:04 +0530

    added edit option in profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import EditProfile from './EditProfile'
+import { useSelector } from 'react-redux'
+
 
 const Profile = () => {
+  const user=useSelector(store=>store.user)
+  if(!user) return <h1>Loading</h1>
   return (
     <div>
-      Profile
+      <EditProfile user={user}/>
     </div>
   )
 }
