17aef44d1d4eb31c86e1e88468b008554ae631a8
charansaigompa <gompacharansai10@gmail.com>
2026-03-22 11:19:53 +0530

    changed socket config for production

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,11 @@
 import { io } from "socket.io-client";
 import { BASE_URL } from "./constants";
 
-export const createSocketConnection=()=>{
-    return io(BASE_URL)
-}
+export const createSocketConnection = () => {
+  if (location.hostname === "localhost") {
+    return io(BASE_URL);
+  } else {
+    return io("/", { path: "/api/socket.io" });
+  }
+};
+    
